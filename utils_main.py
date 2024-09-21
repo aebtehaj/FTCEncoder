5f0e855b85dc246e7f818d37ea8738c0f0adee83
Divya Kumawat <82971883+Divak14@users.noreply.github.com>
2024-09-21 13:06:39 -0400

    Update utils_main.py
    Changed Keras to tensorflow

--- utils_main.py
+++ utils_main.py
@@ -231,23 +231,23 @@ def contrastive_loss_function(xy_true, x_pred, mask_value=999, epsilon=1e-7):
     t = xy_true[:, 1, 3]
     
     # Ensure x_true and x_pred have the same data type
-    x_true = K.cast(x_true, dtype=K.floatx())
-    x_pred = K.cast(x_pred, dtype=K.floatx())
+    x_true = tf.cast(x_true, dtype=tf.float32)
+    x_pred = tf.cast(x_pred, dtype=tf.float32)
     
     # Create a mask to identify padded values
-    mask = K.not_equal(x_true, mask_value)
+    mask = tf.not_equal(x_true, mask_value)
     
-    # Compute the mean squared error with masking
-    mse = K.mean(K.square((x_true - x_pred) * K.cast(mask, dtype=K.floatx())), axis=-1)
+    # Compute the  squared error with masking
+    mse = tf.reduce_mean(tf.square((x_true - x_pred) * tf.cast(mask, dtype=tf.float32)), axis=-1)
     
     # Clip mse to avoid numerical instabilities
-    mse = K.clip(mse, epsilon, 1e10)
+    mse = tf.clip_by_value(mse, epsilon, 1e10)
     
     # Compute the error term
-    err = t[:, np.newaxis] * mse - (1 - t[:, np.newaxis]) * tf.math.log1p(-K.exp(-mse))
+    err = t[:, np.newaxis] * mse - (1 - t[:, np.newaxis]) * tf.math.log1p(-tf.exp(-mse))
     
     # Return the sum of errors
-    return K.sum(err, axis=-1)
+    return tf.reduce_sum(err, axis=-1)
 
 
 def Accuracy(xy_true, x_pred, mask_value=999, epsilon=1e-7):
@@ -269,37 +269,37 @@ def Accuracy(xy_true, x_pred, mask_value=999, epsilon=1e-7):
     t = xy_true[:, 1, 3]
     
     # Ensure x_true and x_pred have the same data type
-    x_true = K.cast(x_true, dtype=K.floatx())
-    x_pred = K.cast(x_pred, dtype=K.floatx())
+    x_true = tf.cast(x_true, dtype=tf.float32)
+    x_pred = tf.cast(x_pred, dtype=tf.float32)
     
     # Create a mask to identify padded values
-    mask = K.not_equal(x_true, mask_value)
+    mask = tf.not_equal(x_true, mask_value)
     
     # Compute the mean squared error with masking
-    mse = K.mean(K.square((x_true - x_pred) * K.cast(mask, dtype=K.floatx())), axis=-1)
-    mse = K.clip(mse, epsilon, 1e10)
+    mse = tf.reduce_mean(tf.square((x_true - x_pred) * tf.cast(mask, dtype=tf.float32)), axis=-1)
+    mse = tf.clip_by_value(mse, epsilon, 1e10)
 
-    prob = K.exp(-mse)
+    prob = tf.exp(-mse)
     # Clip mse to avoid numerical issues
     
     # Compute the error term
     pred_label = prob > 0.5
-    pred_label = K.cast(pred_label, dtype=tf.int32)
+    pred_label = tf.cast(pred_label, dtype=tf.int32)
     
 
     # Replicate true_label to match pred_label's shape
     true_label_replicated = tf.tile(t[:, tf.newaxis], [1, prob.shape[1]])
-    true_label_replicated = K.cast(true_label_replicated, dtype=tf.int32)
+    true_label_replicated = tf.cast(true_label_replicated, dtype=tf.int32)
    
     # Create a mask for the agreement calculation
     cts = tf.not_equal(x_true[:, :, 1], mask_value)
-    cts = K.cast(cts, dtype = tf.int32)
+    cts = tf.cast(cts, dtype = tf.int32)
     # Calculate agreement percentage
     #correct_predictions = tf.reduce_sum(tf.cast(pred_label & cts, dtype= tf.int32) == tf.cast(true_label_replicated & cts, dtype=tf.int32), axis=1)
-    correct_predictions = tf.reduce_sum(tf.cast((pred_label) == (true_label_replicated), dtype=tf.float32) * K.cast(cts, dtype=K.floatx()), axis=1)
+    correct_predictions = tf.reduce_sum(tf.cast((pred_label) == (true_label_replicated), dtype=tf.float32) * tf.cast(cts, dtype=tf.float32), axis=1)
 
     total_predictions = tf.reduce_sum(cts, axis=1)
-    agreement_percentage = tf.cast(correct_predictions, dtype = K.floatx()) / tf.cast(total_predictions, dtype = K.floatx()) * 100
+    agreement_percentage = tf.cast(correct_predictions, dtype = tf.float32) / tf.cast(total_predictions, dtype = tf.float32) * 100
     return tf.reduce_mean(agreement_percentage)
 
 def train_model(
@@ -372,8 +372,8 @@ def compute_accuracy(autoencoder_model, X_data, y_data):
         float: Agreement percentage for the data.
     """
     reconst = autoencoder_model.predict(X_data)
-    mask = K.not_equal(X_data, 999)
-    mse = keras.backend.mean(keras.backend.square((X_data - reconst) * K.cast(mask, dtype=K.floatx())), axis=-1)
+    mask = tf.not_equal(X_data, 999)
+    mse = keras.backend.mean(keras.backend.square((X_data - reconst) * tf.cast(mask, dtype=tf.float32)), axis=-1)
     prob = np.exp(-mse)
     original_array = y_data[:, 1, 3]
     true_label_array = np.tile(original_array[:, None], X_data.shape[1])
@@ -494,4 +494,3 @@ def visualize_probability_time_series(n, original_data, prob, data_var, fs=20, s
 
     plt.tight_layout()
     plt.show()
-
